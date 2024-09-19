4656651c8fb1aa265e278afff1caebdcb8699464
RubaTeixeira <rubateixeira@hotmail.com>
2024-09-19 15:59:12 -0300

    Criando distribuição.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Activity test",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/RubaTeixeira/DIO_img_processing_pckg.git",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
